ce980252797d3bd85ba8a6d4831f4a33f6ca1aeb
QueezZ <queezz.dev@gmail.com>
2025-09-06 20:58:39 +0200

    fix: add auth router

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { testRouter } from './test/test.routes';
 import { questRouter } from './quest/quest.routes';
+import { authRouter } from './auth/auth';
 
 export const router = new OpenAPIHono();
 router.route('/test', testRouter);
 router.route('/quests', questRouter);
+router.route('/auth', authRouter);
