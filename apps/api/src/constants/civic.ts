51921c5635b0030b91898caded61a75e696e80a3
Aciila <vlad.byjik@gmail.com>
2025-09-07 01:45:26 +0200

    add civic login redirect

--- apps/api/src/constants/civic.ts
+++ apps/api/src/constants/civic.ts
@@ -6,10 +6,11 @@ export const civicConfig = () => {
   const clientId = process.env.CIVIC_CLIENT_ID;
   const redirectUrl = process.env.CIVIC_REDIRECT_URL;
   const postLogoutRedirectUrl = process.env.CIVIC_POST_LOGOUT_REDIRECT_URL;
+  const loginSuccessUrl = process.env.CIVIC_LOGIN_URL;
 
-  if (!clientId || !redirectUrl || !postLogoutRedirectUrl) {
+  if (!clientId || !redirectUrl || !postLogoutRedirectUrl || !loginSuccessUrl) {
     throw new Error(
-      'CIVIC_CLIENT_ID or CIVIC_REDIRECT_URL or CIVIC_POST_LOGOUT_REDIRECT_URL is not set'
+      'CIVIC_CLIENT_ID or CIVIC_REDIRECT_URL or CIVIC_POST_LOGOUT_REDIRECT_URL or CIVIC_LOGIN_URL is not set'
     );
   }
 
@@ -17,5 +18,6 @@ export const civicConfig = () => {
     clientId,
     redirectUrl,
     postLogoutRedirectUrl,
+    loginSuccessUrl,
   };
 };
