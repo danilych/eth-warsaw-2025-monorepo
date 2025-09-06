00634500b7ad6e7468de25d8ee547bdb850ddbf2
Aciila <vlad.byjik@gmail.com>
2025-09-06 19:35:02 +0200

    feat/add landing

--- apps/app/src/pages/Index.tsx
+++ apps/app/src/pages/Index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { HeroSection } from '@/components/landing/components/HeroSection';
+import { HowItWorksSection } from '@/components/landing/components/HowItWorksSection';
+import { RewardsSection } from '@/components/landing/components/RewardsSection';
+import { SocialBonusSection } from '@/components/landing/components/SocialBonusSection';
+import { CTASection } from '@/components/landing/components/CTASection';
 
 const Index = () => {
   return (
