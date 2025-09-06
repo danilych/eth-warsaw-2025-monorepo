a92fe73c2b75b0dc1cb33804dd3664ac40548def
QueezZ <queezz.dev@gmail.com>
2025-09-06 03:20:26 +0200

    feat: setup base blockchain parsing logic

---
+++ lib/enums/actions.ts
@@ -0,0 +1,9 @@
+export enum EActions {
+  CREATE_GAME = 'create_game',
+  FINISH_GAME = 'finish_game',
+  CANCEL_GAME = 'cancel_game',
+  PLACE_BET = 'place_bet',
+  WITHDRAW_BET = 'withdraw_bet',
+  WITHDRAW_JACKPOT = 'withdraw_jackpot',
+  CANCEL_BET = 'cancel_bet',
+}
